521581778c738cbca9749e40631e92bb05c7ec90
Dainoras Pusvaskis <dainoras.pusvaskis@atea.lt>
2018-12-05 17:47:11 +0200

    refactor

--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
 'use strict';
-var names = [];
 
 function ready(fn) {
     if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
@@ -9,145 +8,332 @@ function ready(fn) {
     }
 }
 
-function fillData(obj) {
-    document.getElementById('host').value = obj.host;
-    document.getElementById('user').value = obj.user;
-    document.getElementById('password').value = obj.password;
-    document.getElementById('port').value = obj.port;
-}
+function generator() {
+    var names = [];
 
-function getData(obj) {
-    obj.host = document.getElementById('host').value;
-    obj.user = document.getElementById('user').value;
-    obj.password = document.getElementById('password').value;
-    obj.port = document.getElementById('port').value;
-}
-
-function printData(data) {
-    var table = document.getElementById('table');
-    var tr, name, type;
-
-    if (typeof data === 'string') {
-        printFailed(data);
-        return;
-    }
-
-    names = [];
-    table.innerHTML = '';
-    data.forEach(element => {
-        tr = document.createElement('tr');
-        tr.id = element.name;
-        name = document.createElement('td');
-        name.textContent = element.name;
-        type = document.createElement('td');
-        type.textContent = element.type;
-
-        if (element.type === 'd') {
-            tr.addEventListener('click', function () {
-                document.getElementById('path').value = document.getElementById('path').value + '/' + this.id;
-                sendJSON({ list: document.getElementById('path').value }, '/list');
-            });
-
-            tr.classList.add('catalog');
-        } else {
-            tr.classList.add('file');
-        }
-
-        names.push(element.name);
-
-        tr.append(name);
-        tr.append(type);
-        table.append(tr);
-    });
-}
-
-function printFailed(msg) {
-    var table = document.getElementById('table');
-    var tr = document.createElement('tr');
-    var td = document.createElement('td');
-    td.style = 'color: red;';
-    td.textContent = msg || 'server error..';
-    tr.append(td);
-    table.innerHTML = tr.innerHTML;
-}
-
-function sendJSON(obj, url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('POST', url, true);
-    xmlHttp.setRequestHeader('Content-Type', 'application/json');
-    xmlHttp.timeout = 2000;
-
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState === 4) {
-            var response = xmlHttp.responseText;
-            try {
-                response = JSON.parse(response);
-            } catch (error) {
-                // 
-            }
-
-            if (xmlHttp.status === 200) {
-                printData(response);
-            } else {
-                printFailed()
-            }
-        }
-    }
-
-    xmlHttp.send(JSON.stringify(obj));
-}
-
-ready(function () {
     var ftp = {
-        host: 'test.rebex.net',
-        user: 'demo',
-        password: 'password',
+        host: '',
+        user: 'anonymous',
+        pass: '@anonymous',
         port: '21'
     };
 
-    fillData(ftp);
+    var b = (function () {
+        return {
+            connect: document.getElementById('connect'),
+            disconnect: document.getElementById('disconnect'),
+            gotopath: document.getElementById('gotopath'),
+            back: document.getElementById('back'),
+            makehyperlinks: document.getElementById('makehyperlinks')
+        }
+    }());
 
-    document.getElementById('ftp-connect').addEventListener('click', function (event) {
-        getData(ftp);
-        sendJSON(ftp, '/connect');
-        document.getElementById('hyperLinks').innerHTML = '';
-        document.getElementById('path').value = '';
-    });
+    var i = (function () {
+        return {
+            user: document.getElementById('user'),
+            pass: document.getElementById('pass'),
+            host: document.getElementById('host'),
+            port: document.getElementById('port'),
+            path: document.getElementById('path'),
+            hyperLinks: document.getElementById('hyperlinks'),
+            table: document.getElementById('table'),
+            status: document.getElementById('status'),
+            urlExplanation: document.getElementById('urlexplanation')
+        }
+    }());
 
-    document.getElementById('ftp-disconnect').addEventListener('click', function (event) {
-        sendJSON({}, '/disconnect');
-        document.getElementById('hyperLinks').innerHTML = '';
-        document.getElementById('path').value = '';
-    });
+    function clean() {
+        i.urlExplanation.innerHTML = '';
+        i.hyperLinks.innerHTML = '';
+        i.table.innerHTML = '';
+        i.path.value = '';
+    }
 
-    document.getElementById('ftp-path').addEventListener('click', function (event) {
-        var list = document.getElementById('path').value;
-        document.getElementById('hyperLinks').innerHTML = '';
-        sendJSON({ list: list }, '/list');
-    });
-
-    document.getElementById('ftp-back').addEventListener('click', function (event) {
-        var list = document.getElementById('path').value.split('/');
-        list.pop();
-        list = list.join('/')
-        document.getElementById('path').value = list;
-        document.getElementById('hyperLinks').innerHTML = '';
-        sendJSON({ list: list }, '/list');
-    });
-
-    document.getElementById('ftp-hyperlink').addEventListener('click', function (event) {
-        var host = document.getElementById('host').value;
-        var path = document.getElementById('path').value;
-        var hyperLinks = document.getElementById('hyperLinks');
-        var title, url, li;
-
-        hyperLinks.innerHTML = '';
-        names.forEach(function (name) {
-            title = name;
-            url = title.link('https://' + host + path + '/' + name);
-            li = document.createElement('li');
-            li.innerHTML = url;
-            hyperLinks.append(li);
+    function toggleConnection() {
+        document.querySelectorAll('.toggleconnection').forEach(function (v, k) {
+            v.getAttribute('disabled') === '' ? v.removeAttribute('disabled') : v.setAttribute('disabled', '');
         });
+    }
+
+    function printMsg(msg, isSuccess) {
+        var span = document.createElement('span');
+        isSuccess ? span.classList.add('success') : span.classList.add('error');
+        span.textContent = msg;
+        i.status.innerHTML = span.outerHTML;
+    }
+
+    function printFiles(data, server) {
+        var tr, name, type;
+
+        if (typeof data === 'string') {
+            printMsg(data, false);
+            return;
+        }
+
+        names = [];
+        i.table.innerHTML = '';
+
+        data.forEach(element => {
+            tr = document.createElement('tr');
+            tr.id = element.name;
+            name = document.createElement('td');
+            name.textContent = element.name;
+            type = document.createElement('td');
+            type.textContent = element.type;
+
+            if (element.type == '1') {
+                tr.addEventListener('click', function () {
+                    i.path.value = i.path.value + this.id + '/';
+                    server.ls({
+                        ls: i.path.value
+                    }, function (response, isSuccess) {
+                        printFiles(response, server);
+                        isSuccess ? printMsg('Working..', isSuccess) : printMsg(response, isSuccess);
+                    });
+                });
+
+                tr.classList.add('catalog');
+            } else {
+                tr.classList.add('file');
+            }
+
+            names.push(element.name);
+
+            tr.append(name);
+            tr.append(type);
+            table.append(tr);
+        });
+    }
+
+    return {
+        init: function () {
+            i.host.value = ftp.host;
+            i.user.value = ftp.user;
+            i.pass.value = ftp.pass;
+            i.port.value = ftp.port;
+            printMsg('Disconnected..', false);
+            clean();
+        },
+        getData: function () {
+            return {
+                host: i.host.value,
+                user: i.user.value,
+                pass: i.pass.value,
+                port: i.port.value
+            }
+        },
+        getButtons: function () {
+            return b;
+        },
+        connected: function (response, isConnected, server) {
+            if (isConnected) {
+                toggleConnection();
+                clean();
+                printFiles(response, server);
+                printMsg('Connected..', isConnected);
+                i.path.value += '/';
+            } else {
+                printMsg(response, isConnected);
+            }
+        },
+        disconnected: function (response, isConnected) {
+            if (!isConnected) {
+                toggleConnection();
+                clean();
+            }
+
+            printMsg(response, isConnected);
+        },
+        generateHyperlinks: function () {
+            var title, url, li;
+
+            i.hyperLinks.innerHTML = '';
+            i.urlExplanation.innerHTML = 'http:// + Host + Path + Name';
+            names.forEach(function (name) {
+                if (i.path.value[0] !== '/') {
+                    i.path.value = '/' + i.path.value;
+                } else if (i.path.value[i.path.value.length - 1] !== '/') {
+                    i.path.value = i.path.value + '/';
+                }
+                
+                var a = document.createElement('a');
+
+                i.path.value = i.path.value.replace(/\/+/g, '/');
+
+                a.text = name;
+                a.href = 'http://' + i.host.value + i.path.value + name + '/';
+                a.target = '_blank';
+
+                li = document.createElement('li');
+
+                li.innerHTML = a.outerHTML;
+                i.hyperLinks.append(li);
+            });
+        },
+        goToPath: function (server) {
+            i.hyperLinks.innerHTML = '';
+            i.urlExplanation.innerHTML = '';
+
+            server.ls({
+                ls: i.path.value
+            }, function (response, isSuccess) {
+                printFiles(response, server);
+                isSuccess ? printMsg('Working..', isSuccess) : printMsg(response, isSuccess);
+            });
+        },
+        goBack: function (server) {
+            var list = i.path.value.split('/');
+            
+            if (list.length <= 1) return;
+
+            var popped = list.pop();
+
+            if (popped === '') {
+                list.pop();
+            }
+            
+            list = list.join('/')
+
+            if (list[0] !== '/'){
+                list = '/' + list;
+            }
+
+            if (list[list.length - 1] !== '/') {
+                list = list + '/';
+            }
+
+            i.path.value = list;
+            i.hyperLinks.innerHTML = '';
+            i.urlExplanation.innerHTML = '';
+
+            server.ls({
+                ls: list
+            }, function (response, isSuccess) {
+                printFiles(response, server);
+                isSuccess ? printMsg('Working..', isSuccess) : printMsg(response, isSuccess);
+            });
+        }
+    };
+}
+
+function generatorServer() {
+    var response;
+    var rootPath = document.getElementById('rootpath').value;
+    var urls = {
+        connect: '/connect',
+        disconnect: '/disconnect',
+        ls: '/ls'
+    };
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.timeout = 2000;
+
+    return {
+        connect: function (data, cb) {
+
+            xmlHttp.open('POST', urls.connect, true);
+            xmlHttp.setRequestHeader('Content-Type', 'application/json');
+
+            xmlHttp.onreadystatechange = function () {
+                if (xmlHttp.readyState === 4) {
+                    if (xmlHttp.status === 200) {
+                        try {
+                            response = xmlHttp.responseText;
+                            response = JSON.parse(response);
+                            cb(response, true);
+                        } catch (error) {
+                            response = 'Server error.. (JSON)';
+                            cb(response, false);
+                        }
+                    } else {
+                        response = 'Server error.. (status != 200)';
+                        cb(response, false);
+                    }
+                }
+            }
+
+            data.root = rootPath;
+            xmlHttp.send(JSON.stringify(data));
+        },
+        disconnect: function (cb) {
+            xmlHttp.open('POST', urls.disconnect, true);
+            xmlHttp.setRequestHeader('Content-Type', 'application/json');
+
+            xmlHttp.onreadystatechange = function () {
+                if (xmlHttp.readyState === 4) {
+                    if (xmlHttp.status === 200) {
+                        try {
+                            response = xmlHttp.responseText;
+                            cb(response, false);
+                        } catch (error) {
+                            response = 'Server error.. (Disconnect)';
+                            cb(response, false);
+                        }
+                    } else {
+                        response = 'Server error.. (status != 200)';
+                        cb(response, false);
+                    }
+                }
+            }
+
+            xmlHttp.send(JSON.stringify({}));
+        },
+        ls: function (data, cb) {
+            xmlHttp.open('POST', urls.ls, true);
+            xmlHttp.setRequestHeader('Content-Type', 'application/json');
+
+            xmlHttp.onreadystatechange = function () {
+                if (xmlHttp.readyState === 4) {
+                    if (xmlHttp.status === 200) {
+                        try {
+                            response = xmlHttp.responseText;
+                            response = JSON.parse(response);
+                            cb(response, true);
+                        } catch (error) {
+                            response = 'Server error.. (JSON)';
+                            cb(response, false);
+                        }
+                    } else {
+                        response = 'Server error.. (status != 200)';
+                        cb(response, false);
+                    }
+                }
+            }
+
+            data.root = rootPath;
+            xmlHttp.send(JSON.stringify(data));
+        }
+    };
+}
+
+ready(function () {
+    var gen = generator();
+    var server = generatorServer();
+    var buttons = gen.getButtons();
+
+    gen.init();
+
+    buttons.connect.addEventListener('click', function (event) {
+        server.connect(gen.getData(), function (response, isSuccess) {
+            gen.connected(response, isSuccess, server);
+        });
+    });
+
+    buttons.disconnect.addEventListener('click', function (event) {
+        server.disconnect(function (response, isSuccess) {
+            gen.disconnected(response, isSuccess);
+        });
+    });
+
+    buttons.gotopath.addEventListener('click', function (event) {
+        gen.goToPath(server);
+    });
+
+    buttons.back.addEventListener('click', function (event) {
+        gen.goBack(server);
+    });
+
+    buttons.makehyperlinks.addEventListener('click', function (event) {
+        gen.generateHyperlinks();
     });
 });
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const parser = require('body-parser');
-const ftp = require('promise-ftp');
+const jsftp = require('jsftp');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-let client = new ftp();
+
+let ftps = undefined;
 
 app.use(express.static(__dirname + '/public'));
 app.use(parser.json());
@@ -17,49 +18,100 @@ app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/connect', async (req, res) => {
+app.post('/connect', (req, res) => {
     const data = req.body;
-    console.log('data?: ', data);
+    let interval;
     let items = [];
+    let counter = 5;
 
-    if (client.getConnectionStatus() === 'connected') {
-        console.log('connected..');
-        await client.end();
+    if (data.host === '' || data.port === '' || data.user === '' || data.pass === '') {
+        res.send('Noup.. ');
+        return;
     }
 
-    client.connect({
-        host: data.host,
-        user: data.user,
-        password: data.password,
-        port: data.port,
-        connTimeout: 1000
-    }).then((msg) => {
-        console.log('msg: ', msg, client);
-        return client.list('/');
-    }).then((list) => {
-        console.log('LIST?');
-        items = list;
-    }).finally(() => {
-        res.send(items);
-    });
-});
+    try {
+        ftps = new jsftp({
+            host: data.host,
+            port: data.port,
+            user: data.user,
+            pass: data.pass,
+            timeout: 2
+        });
 
-app.post('/list', (req, res) => {
-    const data = req.body;
-    let items = [];
+        ftps.ls('/' + data.root, function (noup, files) {
+            if (typeof files !== 'undefined') {
+                items = files;
+            }
+        });
 
-    client.list(
-        data.list
-    ).then((list) => {
-        items = list;
-    }).finally(() => {
-        res.send(items);
-    });
+        interval = setInterval(function () {
+            counter--;
+            if (items.length > 0 || counter === 0) {
+                console.log("Connected. Hi!");
+                res.send(items);
+                clearInterval(interval);
+            }
+        }, 400);
+    } catch (error) {
+        res.send('500 in server(connect).. ' + error);
+    }
 });
 
 app.post('/disconnect', (req, res) => {
-    client.destroy();
-    res.send('disconnected');
+    let isQuit = false;
+    let counter = 5;
+    let interval;
+
+    try {
+        ftps.raw('quit', (err, data) => {
+            if (err) {
+                res.send('quit failed..');
+                clearInterval(interval);
+                return console.error(err);
+            }
+
+            console.log("Disconnected. Bye!");
+            isQuit = true;
+        });
+
+        interval = setInterval(function () {
+            counter--;
+            if (isQuit || counter === 0) {
+                res.send('Disconnected..');
+                clearInterval(interval);
+            }
+        }, 400);
+    } catch (error) {
+        res.send('500 in server(disconnect).. ' + error);
+    }
+});
+
+app.post('/ls', (req, res) => {
+    const data = req.body;
+    let interval;
+    let items = [];
+    let counter = 5;
+
+    console.log(data.ls);
+
+    try {
+        ftps.ls('/' + data.root + data.ls, function (noup, files) {
+            if (typeof files !== 'undefined') {
+                items = files;
+            }
+        });
+
+        interval = setInterval(function () {
+            counter--;
+            if (items.length > 0 || counter === 0) {
+                console.log('ls: ', items);
+                res.send(items);
+                clearInterval(interval);
+            }
+        }, 400);
+    } catch (error) {
+        res.send('500 in server(connect).. ' + error);
+    }
 });
 
 app.listen(PORT, () => {
